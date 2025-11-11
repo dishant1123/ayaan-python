41c316e5c232d7e588234b0b83d2bc4f24624a71
dishant1123 <dishantshah1417@gmail.com>
2025-11-11 17:34:27 +0530

    ....

--- 11nov.py
+++ 11nov.py
@@ -55,6 +55,7 @@ t.display()
 
 #ex :3 
 
+"""
 class emp : 
     def __init__(self):
         self.x =1
@@ -64,12 +65,44 @@ class emp :
         
 class manager(emp):
     def __init__(self):
-        super().__init__()
+        super().__init__() 
     def show(self):
         self.x =self.x+1 
         print(self.x) 
 
 m=manager()
 m.show()       
-
+"""
 # option  a. 11   b.2   c.1   d none 
+
+# 2. multiple  level inheritance  : 
+
+"""
+class a 
+
+class b : 
+
+class c(a,b):    # MRO  : ==> method resolution order
+"""
+
+class father :
+    def __init__(self,f_name):
+        self.f_name =f_name 
+
+class mother :
+    def __init__(self,m_name):
+        self.m_name =m_name
+        
+class son(father,mother):
+    def __init__(self,f_name,m_name,s_name):
+        father.__init__(self,f_name)
+        mother.__init__(self,m_name)
+        self.s_name =s_name
+    
+    def display(self):
+        print("son name is  :",self.s_name)
+        print("father name is  :",self.f_name)
+        print("mother name is  :",self.m_name)
+
+s=son("sangin","labdhi","ayaan")
+s.display()
